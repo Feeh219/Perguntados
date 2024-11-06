63b6a24c9dcd15e964bc29abbdf2e2e162985979
Feeh219 <168578282+Feeh219@users.noreply.github.com>
2024-11-06 10:56:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'MATEMÁTICA',
+    'Qual é a raiz quadrada de 4?',
+    '2'
+)
+
+criaCartao(
+    'MATEMÁTICA',
+    'Quanto é 7 vezes 8?',
+    '56'
+)
+
+criaCartao(
+    'KIMETSU NO YAIBA',
+    'O que inspirou o estilo de luta dos personagens, como as Técnicas de respiração?',
+    'Gotouge baseou as Técnicas de Respiração em elementos da cultura japonesa e artes marciais tradicionais, criando uma habilidade única para cada personagem, inspirada em elementos da natureza, como Água, Fogo e Relâmpago.'
+)
+
+criaCartao(
+    'KIMETSU NO YAIBA',
+    'Qual personagem possui um spin-off próprio?',
+    'Kyojuro Rengoku, o Hashira das Chamas, possui um spin-off chamado Kimetsu no Yaiba Gaiden, que explora mais sobre sua história e suas aventuras como Hashira.'
+)
